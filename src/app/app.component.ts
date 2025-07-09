6aba1fbdf0b432b3db6102b3c294a68a1e2b65f9
aiemfalco <f.falcone21@studenti.uniba.it>
2025-07-09 18:32:17 +0200

    aggiunta possibilità di cambiare tema dinamicamente tramite select dei presets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,27 @@ import { RouterModule } from '@angular/router';
 import { PrimeNG } from 'primeng/config';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, ButtonModule, CommonModule],
+  imports: [RouterModule, ButtonModule, CommonModule, DropdownModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
 
   isDark = false;
-
+  defaultTheme: string = 'nora';
+  
   constructor(private _primeng: PrimeNG) {}
+
+  toggleTheme() {
+    this.isDark = !this.isDark;
+    document.body.classList.toggle('dark-mode', this.isDark);
+    localStorage.setItem('dark-mode', this.isDark ? 'true' : 'false');
+  }
 
   ngOnInit(): void {
       this._primeng.ripple.set(true);
@@ -22,11 +31,21 @@ export class AppComponent implements OnInit {
       if (this.isDark) {
         document.body.classList.add('dark-mode');
     }
+    // ripristina il tema salvato al load
+    const saved = localStorage.getItem('theme') || 'nora';
+    this.changeTheme(saved);
   }
 
-  toggleTheme() {
-    this.isDark = !this.isDark;
-    document.body.classList.toggle('dark-mode', this.isDark);
-    localStorage.setItem('dark-mode', this.isDark ? 'true' : 'false');
+  onThemeChange(event: Event) {
+  const value = (event.target as HTMLSelectElement).value;
+  if (value) {
+    this.changeTheme(value);
   }
+}
+
+  changeTheme(presetName: string) {
+    this._primeng.theme.set(presetName);
+    localStorage.setItem('theme', presetName);
+  }
+
 }
