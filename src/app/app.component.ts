e2083b080a8c28f4bd65e2037675f3389e373e67
aiemfalco <f.falcone21@studenti.uniba.it>
2025-07-16 11:42:21 +0200

    MyPreset è il default theme, inoltre è stato aggiunto tra le opzioni di scelta del tema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,33 +3,19 @@ import { RouterModule } from '@angular/router';
 import { PrimeNG } from 'primeng/config';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
-import { DropdownModule } from 'primeng/dropdown';
 import { FormsModule } from '@angular/forms';
-import { usePreset } from '@primeng/themes';
-import { Select } from 'primeng/select';
-
-import Aura from '@primeng/themes/aura';
-import Lara from '@primeng/themes/lara';
-import Material from '@primeng/themes/material';
-import Nora from '@primeng/themes/nora'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, ButtonModule, CommonModule, DropdownModule, FormsModule, Select],
+  imports: [RouterModule, ButtonModule, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
 
   isDark = false;
-  actualTheme: object = Aura
-  themes = [
-    { label: 'Aura', value: Aura }, 
-    { label: 'Lara', value: Lara }, 
-    { label: 'Material', value: Material }, 
-    { label: 'Nora', value: Nora }
-  ];
+  number = 0;
 
   constructor(private _primeng: PrimeNG) {
   }
@@ -46,21 +32,9 @@ export class AppComponent implements OnInit {
       if (this.isDark) {
         document.body.classList.add('dark-mode');
       }
-      // applica l'ultimo tema salvato
-      const storageItem = localStorage.getItem('theme');
-      if (storageItem) {
-        console.log("Tema caricato al log: ", storageItem);
-        this.changeTheme(this.themes.find(theme => theme.label === storageItem)!.value)
-      }
     }
-    
-  async changeTheme(preset: object) {
-      try {
-        await usePreset(preset);
-        this.actualTheme = preset
-        console.log('Preset' , preset, "applicato");
-      } catch (err) {
-        console.error('Errore nel cambio preset:', err);
-      }
+
+  counter(): void {
+    console.log(this.number++);
   }
 }
--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -3,15 +3,23 @@ import { User } from '../user.model';
 import { UserService } from '../user.service';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { ChangeDetectionStrategy } from '@angular/core';
 
+import { Select } from 'primeng/select';
+import { usePreset } from '@primeng/themes';
+import Aura from '@primeng/themes/aura';
+import Lara from '@primeng/themes/lara';
+import Material from '@primeng/themes/material';
+import Nora from '@primeng/themes/nora'
+import { MyPreset } from '../../../mypreset';
+
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [CommonModule, RouterModule, ReactiveFormsModule, ButtonModule, TableModule],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, ButtonModule, TableModule, Select, FormsModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -19,6 +27,14 @@ import { ChangeDetectionStrategy } from '@angular/core';
 export class UserListComponent implements OnInit {
 
   users: User[] = []
+  actualTheme: object = Aura;
+  themes = [
+    { label: 'Aura', value: Aura }, 
+    { label: 'Lara', value: Lara }, 
+    { label: 'Material', value: Material }, 
+    { label: 'Nora', value: Nora },
+    { label: 'MyPreset', value: MyPreset}
+  ];
 
   constructor(
     private _userService: UserService, 
@@ -27,13 +43,24 @@ export class UserListComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.loadUsers();
-    }
+    this.loadUsers(); // questo fa cambiare l'input(users era vuoto) -> il component diventa dirty quindi deve essere ricontrollato
+    const defaultTheme = 'MyPreset'; //const storageItem = localStorage.getItem('theme');
+    this.changeTheme(this.themes.find(theme => theme.label === defaultTheme)?.value);
+  }
+
+   async changeTheme(preset: object) {
+      try {
+        await usePreset(preset);
+        this.actualTheme = preset;
+      } catch (err) {
+        console.error('Errore nel cambio preset:', err);
+      }
+  }
 
   loadUsers(): void {
-    this._userService.getUsers().subscribe(data => {
-      this.users = data;
-      this._cd.markForCheck();
+    this._userService.getUsers().subscribe(data => { // chiamata HTTP
+      this.users = data; // users valorizzato
+      this._cd.markForCheck(); // fa scattare i trigger che non scattano con l'onPush e aggiorna quindi il DOM, senza di essa users non viene visualizzato nel template
     });
   }
 
